36117a62ccf527b834841aa3bc624e2ad40b256d
manu97galicia-maker <manu97galicia@gmail.com>
2026-02-10 16:46:33 +0100

    Remove routing and render App directly

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Terminos from './pages/terminos'
-import Privacidad from './pages/privacidad'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/terminos" element={<Terminos />} />
-        <Route path="/privacidad" element={<Privacidad />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
